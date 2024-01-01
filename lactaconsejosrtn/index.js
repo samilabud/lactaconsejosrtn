82e04bf4eb38c8e8625efcd2b4af076c1d14f19a
Samil Abud <samilabud@gmail.com>
2024-01-01 14:08:11 -0400

    Fixed time to redirect

--- lactaconsejosrtn/index.js
+++ lactaconsejosrtn/index.js
@@ -2,7 +2,7 @@ const launchApp = (deepLink = "", fallBack = "") => {
   var now = new Date().valueOf();
   setTimeout(function () {
     window.location = fallBack;
-  }, 10000);
+  }, 1000);
   window.location = deepLink;
 };
 
