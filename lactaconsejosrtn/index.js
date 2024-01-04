4dceb50bfe34d255142aaca03b630ecbf26b0785
Samil Abud <samilabud@gmail.com>
2024-01-03 21:04:09 -0400

    Adapting to OG Open Graph Protocol and added images

--- lactaconsejosrtn/index.js
+++ lactaconsejosrtn/index.js
@@ -1,9 +1,9 @@
 const launchApp = (deepLink = "", fallBack = "") => {
   var now = new Date().valueOf();
   setTimeout(function () {
-    window.location = fallBack;
+    // window.location = fallBack;
   }, 4000);
-  window.location = deepLink;
+  // window.location = deepLink;
 };
 
 const deepLink = "lactaconsejos://a" + window.location.pathname;
